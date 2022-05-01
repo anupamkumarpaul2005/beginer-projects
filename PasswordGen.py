8e45a87dece9da798ed9d4c966a380029b48c811
Anupam Kumar Paul <anupamkumarpaul2005@gmail.com>
2022-05-01 08:51:50 +0530

    Program Half completed

---
+++ PasswordGen.py
@@ -0,0 +1,38 @@
+import random
+
+print("""
+                PASWORD GENERATOR
+                
+        This will help you generate a password of any length.
+        
+        To Start, type 'Generate'
+        To Change Settings, type 'Settings'
+        To quit, type 'Quit'
+        
+""")
+data = [n for n in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTVWXYZ01234567890!@#$%^&*()?.,:"]
+
+
+def gen():
+    print("Enter length of password: ")
+    l = int(input())
+    pswrd = "".join(random.sample(data, l))
+    print("Your pssword can be: "+pswrd)
+
+
+def settings():
+    print("""
+            Upper Case Alphabets:   YES                          Lower Case Alphabets: YES
+            Numbers:                YES                          Special Characters:   !@#$%^&*()?.,:
+    """)
+    print("You want to add  or remove ")
+
+
+while True:
+    query = input().upper()
+    if query == 'QUIT' or query == 'Q':
+        exit()
+    elif query == 'GENERATE' or query == 'G':
+        gen()
+    else:
+        print("Not a valid request.")
